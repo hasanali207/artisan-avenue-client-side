f2984cf83948566189dafee2d57dfbe4347ae785
hasanali207 <hasanali142228@gmail.com>
2024-04-30 21:39:57 +0600

    added data for sbumit

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,29 +2,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// require('dotenv').config(); // Load environment variables from .env file
-
 
 
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAzmbJro2RdckCNsoJSkp-9Wem7HTMlrCM",
-  authDomain: "artisan-avenue-fe34e.firebaseapp.com",
-  projectId: "artisan-avenue-fe34e",
-  storageBucket: "artisan-avenue-fe34e.appspot.com",
-  messagingSenderId: "73330597114",
-  appId: "1:73330597114:web:d0a04d6171be5010ad7d43"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
-
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID
-// };
 
 
 const app = initializeApp(firebaseConfig);
