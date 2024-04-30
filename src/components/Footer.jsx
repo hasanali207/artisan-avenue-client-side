d353ace742f195062107bf966ac17e4bc9e202f4
hasanali207 <hasanali142228@gmail.com>
2024-04-30 22:58:53 +0600

    added footer data

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,16 @@
 // Footer.js
 import React from 'react';
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
+
 function Footer() {
   return (
-    <footer className="footer mt-10 p-10 bg-base-200 text-base-content">
+    <>
+    <footer className="footer mt-10  text-black p-10 bg-slate-100">
   <nav>
-    <h6 className="footer-title">Services</h6> 
-    <a className="link link-hover">This is ArtCraft Website</a>
+    <h6 className="footer-title">Artisan Avenue</h6> 
+    <a className="link link-hover">Word First Class Art & Craft Services.</a>
     
   </nav> 
   <nav>
@@ -33,7 +38,26 @@ function Footer() {
       </div>
     </fieldset>
   </form>
+
+
 </footer>
+
+{/* bottom Footer */}
+<footer className="footer items-center text-white p-4 bg-slate-500 ">
+  <aside className="items-center grid-flow-col">
+    
+    <p>Copyright © 2024 - All right reserved</p>
+  </aside> 
+  <nav className="grid-flow-col text-xl text-white gap-4 md:place-self-center md:justify-self-end">
+    <a>
+      <FaFacebook></FaFacebook>
+    </a>
+    <a> <FaGithub></FaGithub></a>
+    <a><FaLinkedin></FaLinkedin></a>
+  </nav>
+</footer>
+
+</>
   );
 }
 
